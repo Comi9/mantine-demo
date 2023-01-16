96d3ea367b8e7a7a9c6f66f92303190330c8b0c9
Cosmin Popescu <cosminp9@gmail.com>
2023-01-16 19:01:07 +0200

    Add Mantine UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,39 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { GetServerSidePropsContext } from 'next'
+import { useState } from 'react'
+import { AppProps } from 'next/app'
+import { getCookie, setCookie } from 'cookies-next'
+import {
+  MantineProvider,
+  ColorScheme,
+  ColorSchemeProvider
+} from '@mantine/core'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+  const { Component, pageProps } = props
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme)
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark')
+    setColorScheme(nextColorScheme)
+    setCookie('mantine-color-scheme', nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30
+    })
+  }
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  )
 }
+
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light'
+})
